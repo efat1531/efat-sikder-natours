fa36b8790252e22604839f21591d6cfb52cceb47
Sp3cTer <20103033@iubat.edu>
2024-04-05 16:09:05 +0600

    Added Reset Password Functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,13 @@
 /* eslint-disable prettier/prettier */
 const jwt = require("jsonwebtoken");
 const util = require("util");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const moment = require("moment");
+const crypto = require("crypto");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const sendEmail = require("../utils/email");
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -89,3 +93,111 @@ exports.restrictTo =
     }
     next();
   };
+
+exports.forgetPassword = catchAsync(async (req, res, next) => {
+  if (!req.body.email) {
+    return next(new AppError("Please provide an email!", 400));
+  }
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+  if (!user) {
+    return next(new AppError("User with this email does not exist!", 404));
+  }
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/resetPassword/${resetToken}`;
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token (valid for 15 minutes)",
+      message,
+    });
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+      resetToken,
+    });
+  } catch (error) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+    return next(
+      new AppError(
+        "There was an error sending the email. Try again later!",
+        500
+      )
+    );
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  if (!req.body.password || !req.body.passwordConfirm) {
+    return next(
+      new AppError("Please provide password and passwordConfirm!", 400)
+    );
+  }
+
+  if (req.body.password !== req.body.passwordConfirm) {
+    return next(new AppError("Passwords do not match!", 400));
+  }
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  }).select("+password");
+  if (!user) {
+    return next(new AppError("Token is invalid or has expired!", 400));
+  }
+  const previousPassword = await user.usedPreviousPassword(
+    req.body.password,
+    user.password
+  );
+  if (previousPassword) {
+    return next(
+      new AppError("The reset password and current password are same!", 400)
+    );
+  }
+  const usedPass = await user.previousUsedPassword(req.body.password);
+  if (usedPass) {
+    let message;
+    const passDate = moment(usedPass);
+    const now = moment();
+    const difference = now.diff(passDate);
+
+    if (moment.duration(difference).asDays() > 1) {
+      const daysAgo = Math.floor(moment.duration(difference).asDays());
+      message = `This password was changed ${daysAgo} days ago`;
+    } else if (moment.duration(difference).asHours() > 1) {
+      const hoursAgo = Math.floor(moment.duration(difference).asHours());
+      message = `This password was changed ${hoursAgo} hours ago`;
+    } else if (moment.duration(difference).asMinutes() > 1) {
+      const minutesAgo = Math.floor(moment.duration(difference).asMinutes());
+      message = `This password was changed ${minutesAgo} minutes ago`;
+    } else {
+      const secondsAgo = Math.floor(moment.duration(difference).asSeconds());
+      message = `This password was changed ${secondsAgo} seconds ago`;
+    }
+    return next(new AppError(`${message}`, 400));
+  }
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordChangedAt = Date.now();
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
